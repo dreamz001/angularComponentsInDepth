2f2336d8091abc83e1c97e60ae619f9c794d8de4
Rajinder Singh <rajinders@chetu.com>
2021-04-30 21:17:52 +0530

    chapter-4 done

---
+++ src/app/blog-post-tile/blog-post-tile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-tile',
+  templateUrl: './blog-post-tile.component.html',
+  styleUrls: ['./blog-post-tile.component.scss']
+})
+export class BlogPostTileComponent implements OnInit {
+
+  title: string;
+  summary: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.title = "Blog Title";
+    this.summary = "Blog Post Summary"
+  }
+
+}
