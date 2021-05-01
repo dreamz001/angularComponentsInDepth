3c4df571c09f583a79dd0dc1740b57eaaf36e232
Red Love <dreamzfulfilled@gmail.com>
2021-05-01 06:39:57 +0530

    Chapter-10 done.

--- src/app/blog-post-tile/blog-post-tile.component.ts
+++ src/app/blog-post-tile/blog-post-tile.component.ts
@@ -10,10 +10,16 @@ import { TruncatePipe } from '../truncate.pipe';
 export class BlogPostTileComponent implements OnInit {
 
  @Input() post: BlogPost;
+  fullSummary :string;
   constructor(private trucatePipe: TruncatePipe ) { }
 
   ngOnInit() {
+    this.fullSummary = this.post.summary;
     this.post.summary = this.trucatePipe.transform(this.post.summary,[50])
+  }
+
+  showFullSummary(){
+    this.post.summary = this.fullSummary;
   }
 
 }
