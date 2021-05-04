0205d6de6619fb9cca3f81eb10104ee33d8f5aa9
Red Love <dreamzfulfilled@gmail.com>
2021-05-04 22:20:47 +0530

    Chapter-16 done.

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { title } from 'process';
+import { BlogDataService } from '../blog-data.service';
 import { BlogPost } from '../blog-post';
 import { BlogPostTileComponent } from '../blog-post-tile/blog-post-tile.component';
 
@@ -13,14 +14,15 @@ export class BlogListComponent implements OnInit {
   blogPosts: BlogPost[][];
   currentPage: number;
   @ViewChildren('tile') blogPostTileComponents: QueryList<BlogPostTileComponent>;
-  constructor() { }
+  constructor(public blogDataService: BlogDataService) { }
   
   ngOnInit() {
     this.currentPage = 0;
+    this.blogPosts = this.blogDataService.getData();
   //  this.blogPosts.push(new BlogPost("Blog Post 1","Summary 1"));
    // this.blogPosts.push(new BlogPost("Blog Post 2","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
    // this.blogPosts.push(new BlogPost("Blog Post 3","Summary 3"));
-   this.blogPosts = [
+   /* this.blogPosts = [
      [
       {title:"Blog Post 1", summary:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
       {title:"Blog Post 2", summary:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
@@ -39,7 +41,7 @@ export class BlogListComponent implements OnInit {
       {title:"Blog Post 11", summary:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
       {title:"Blog Post 12", summary:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
     ]
-   ]
+   ] */
   }
 
   updatePage(newPage){
